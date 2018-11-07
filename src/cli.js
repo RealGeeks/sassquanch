ea596841419e6a297f1f31a97a25190cf9235c16
Jorge Valdez <jorge@realgeeks.com>
2018-11-07 14:51:16 -0600

    prepare for publishing

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ import { fileExists, DIE } from './util';
 
 export const CLI = program
   .name('sassquanch')
-  .version('0.1.0')
+  .version('0.0.1')
   .usage('[options] <output-path>')
   .option(
     '-p, --package-path <path>',
