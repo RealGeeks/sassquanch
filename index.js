0b147bd4e1779e8f81f0c15dfe50d98eaf88b31b
Jorge Valdez <jorge@realgeeks.com>
2018-11-07 14:01:20 -0600

    added bin run script

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import { parseOpts } from './src';
+import { cli, deps } from './src';
 
 if (process.mainModule) {
-  const { outputPath, packagePath } = parseOpts();
-  console.log(`${outputPath}, ${packagePath}`);
+  const { outputPath, packagePath } = cli.parseOpts(); // eslint-disable-line
+
+  const packageInfo = deps.readPackage(packagePath);
+  const styleDeps = deps.getStyleDependencies(packageInfo);
+  const resolvedDeps = deps.resolveStyleDependencies(styleDeps, packagePath);
+
+  console.dir(resolvedDeps, outputPath);
 }
 else {
   console.log('required as a module');
